2aa5d434ae74ec7ae823b030aad0be9432280fb6
httpLucasReis <manoel.reisiema@gmail.com>
2020-07-30 23:59:31 -0300

    Utilizando classes no es6

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
-class Teste { 
-    metodo() {
+// criando um método estático 
 
-    }
-
-    outroMetedo(){
-
+class Matematica { 
+    static soma(a,b){
+        return a+b;
     }
 }
+
+// Herança (Estendendo uma classe)
+class List {
+    constructor() {
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data)
+    }
+
+}
+
+
+// Criando uma classe
+class TodoList extends List{ 
+    constructor(){
+      super(); // Chamando o constructor da classe pai  
+
+      this.usuario = 'Diego';
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+    
+}
+
+// Instanciando um objeto
+const MinhaLista = new TodoList();
+
+// Chamando um método
+document.getElementById("novotodo").onclick = function(){
+    MinhaLista.add('Novo todo');
+}
+
+console.log(Matematica.soma(2,5));
